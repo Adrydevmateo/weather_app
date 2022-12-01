32a785abf02634b42f4ee6132679f84a87ee0842
Adrydevmateo <adrydevmateo@gmail.com>
2022-12-01 14:09:50 -0400

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Weather - Projects</title>
+        <meta name="author" content="AdryDev | Adry Mateo Ramon" />
+        <meta
+          name="description"
+          content="Weather application using a free api"
+        />
+        <link
+          rel="icon"
+          href="https://images.unsplash.com/uploads/141362941583982a7e0fc/abcfbca1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+        />
+      </Head>
+
+      <main></main>
+    </>
+  );
+}
