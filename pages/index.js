36b3c438b8dd9af8caefb4fe7d1fc53190fe7f55
Adrydevmateo <adrydevmateo@gmail.com>
2022-12-01 15:38:53 -0400

    fetching data successfully

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 import Head from "next/head";
 import Image from "next/image";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Home() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const uri = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+
+  const fetchWeather = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios.get(uri).then((response) => setWeather(response.data));
+    setCity("");
+    setLoading(false);
+  };
+
   return (
     <>
       <Head>
@@ -11,13 +27,19 @@ export default function Home() {
           name="description"
           content="Weather application using a free api"
         />
-        <link
-          rel="icon"
-          href="https://images.unsplash.com/uploads/141362941583982a7e0fc/abcfbca1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-        />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </Head>
 
-      <main></main>
+      <main>
+        <form onSubmit={fetchWeather} action="#">
+          <input
+            onChange={({ target }) => setCity(target.value)}
+            placeholder="Search City"
+            type="text"
+          />
+          <button onClick={fetchWeather}>Click</button>
+        </form>
+      </main>
     </>
   );
 }
